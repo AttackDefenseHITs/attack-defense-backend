3cfb5d000ac73d1f13a66f5a34b28bf19b4e2798
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2024-11-03 17:07:53 +0700

    Исправлены миграции

--- src/main/resources/migration/V2__create_team_tables.sql
+++ src/main/resources/migration/V2__create_team_tables.sql
@@ -1,11 +1,11 @@
 CREATE TABLE teams (
-       id UUID PRIMARY KEY DEFAULT,
+       id UUID PRIMARY KEY,
        name VARCHAR(255) NOT NULL,
        max_members BIGINT NOT NULL
 );
 
 CREATE TABLE team_members (
-       id UUID PRIMARY KEY DEFAULT,
+       id UUID PRIMARY KEY,
        user_id UUID NOT NULL,
        team_id UUID NOT NULL,
        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
