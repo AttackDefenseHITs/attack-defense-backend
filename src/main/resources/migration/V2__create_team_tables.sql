d926c28a4cfc1ffa4a7a4d5bba0c6b20d1327bb2
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2024-11-03 15:00:58 +0700

    Добавлены миграции для команд

---
+++ src/main/resources/migration/V2__create_team_tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE teams (
+       id UUID PRIMARY KEY DEFAULT,
+       name VARCHAR(255) NOT NULL,
+       max_members BIGINT NOT NULL
+);
+
+CREATE TABLE team_members (
+       id UUID PRIMARY KEY DEFAULT,
+       user_id UUID NOT NULL,
+       team_id UUID NOT NULL,
+       FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+       FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE
+);
