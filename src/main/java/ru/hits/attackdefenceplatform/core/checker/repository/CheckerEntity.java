6a92f7635b9ef8e7e581d93ee2bba9876571e78f
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-01-14 18:12:03 +0700

    Добавлена автогенерация идентификатора для чекера

--- src/main/java/ru/hits/attackdefenceplatform/core/checker/repository/CheckerEntity.java
+++ src/main/java/ru/hits/attackdefenceplatform/core/checker/repository/CheckerEntity.java
@@ -3,6 +3,8 @@ package ru.hits.attackdefenceplatform.core.checker.repository;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -17,6 +19,7 @@ import java.util.UUID;
 @Data
 public class CheckerEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @OneToOne(fetch = FetchType.LAZY)
