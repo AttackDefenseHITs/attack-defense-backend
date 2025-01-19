6f2db7ace4e729ef044621bea92524b63f3a4bf9
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-01-19 21:00:58 +0700

    Изменены директории в core/checker

--- src/main/java/ru/hits/attackdefenceplatform/core/checker/handler/CheckerResultHandler.java
+++ src/main/java/ru/hits/attackdefenceplatform/core/checker/handler/CheckerResultHandler.java
@@ -1,4 +1,4 @@
-package ru.hits.attackdefenceplatform.core.checker;
+package ru.hits.attackdefenceplatform.core.checker.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
--- src/main/java/ru/hits/attackdefenceplatform/core/checker/script/CheckerValidator.java
+++ src/main/java/ru/hits/attackdefenceplatform/core/checker/script/CheckerValidator.java
@@ -1,4 +1,4 @@
-package ru.hits.attackdefenceplatform.core.checker;
+package ru.hits.attackdefenceplatform.core.checker.script;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
