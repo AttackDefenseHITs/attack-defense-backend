9119d65a940615b8ceb2a276564669001daf0c92
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-05-12 14:10:42 +0700

    Исправлены настройки cors

--- src/main/java/ru/hits/attackdefenceplatform/configuration/SpringSecurityConfiguration.java
+++ src/main/java/ru/hits/attackdefenceplatform/configuration/SpringSecurityConfiguration.java
@@ -59,7 +59,7 @@ public class SpringSecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         var configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://46.29.239.208"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://46.29.239.208", "https://hits-ad.ru"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
