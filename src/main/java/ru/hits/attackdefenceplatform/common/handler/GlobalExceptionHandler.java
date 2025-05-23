c0ff8f46e9f75896fdb50aa7aedd1cbb0ebeca87
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2025-05-23 17:56:13 +0700

    Little fix

--- src/main/java/ru/hits/attackdefenceplatform/common/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/hits/attackdefenceplatform/common/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.hits.attackdefenceplatform.common.exception.CompetitionException;
@@ -15,9 +16,25 @@ import ru.hits.attackdefenceplatform.common.exception.flag.FlagExpiredException;
 import ru.hits.attackdefenceplatform.common.exception.flag.InvalidFlagException;
 import ru.hits.attackdefenceplatform.common.exception.flag.OwnFlagSubmissionException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        log.warn("Ошибка валидации: {}", errors);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(FlagExpiredException.class)
     public ResponseEntity<String> handleFlagExpiredException(FlagExpiredException ex) {
         return ResponseEntity.status(HttpStatus.GONE).body(ex.getMessage());
